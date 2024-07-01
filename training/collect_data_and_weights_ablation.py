1e0ee64e792878bb49181f24c68cf95437eb8f74
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2024-07-01 11:24:09 +0200

    fix: the domain proportion for programming language was not normalized

--- training/collect_data_and_weights_ablation.py
+++ training/collect_data_and_weights_ablation.py
@@ -258,7 +258,8 @@ if __name__ == "__main__":
         prefix = os.path.join(args.folder, prefix)
         name = prefix_to_canonical_name(prefix, stats_datasets)
         if name not in stats_datasets:
-            raise RuntimeError(f"Dataset {name} cannot be matched ({prefix=})")
+            #  throw an error with the new CulturaX dataset for now
+            # raise RuntimeError(f"Dataset {name} cannot be matched ({prefix=})")
             continue
         if name in not_tokenized_datasets:
             not_tokenized_datasets.remove(name)
@@ -298,7 +299,6 @@ if __name__ == "__main__":
             num_tokens_per_programming_language[prog_lang] = (
                 num_tokens_per_programming_language.get(prog_lang, 0) + count
             )
-    print(json.dumps(data, indent=4))
 
     if not_tokenized_datasets and args.debug:
         print(
@@ -348,7 +348,7 @@ if __name__ == "__main__":
         ), f"{language=} not found"
         target_proportion = (
             programming_language_target_proportions[language]
-            * domain_target_proportions["code--programming"]
+            * normalized_domain_target_proportions["code--programming"]
         )
         weight = target_proportion / (count_weighted / total_count_weighted)
         programming_language_weights[language] = weight
