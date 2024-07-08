7a779d305ce6152aa10cfa2d6b8e7e488b3e8530
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2024-07-08 13:34:45 +0200

    feat: improve ablation study experiments management

---
+++ ablation/models_config/lucie410m.sh
@@ -0,0 +1,14 @@
+# Variables for the Lucie80M architecture
+export PP=2
+export TP=1
+
+export HIDDEN_SIZE=1024
+export FFN_HIDDEN_SIZE=3072
+export NUM_LAYERS=24
+export NUM_HEADS=16
+export NUM_KV_HEADS=4
+export SEQ_LENGTH=4096
+
+export GLOBAL_BATCH_SIZE=256
+export MICRO_BATCH_SIZE=4
+export ZERO_STAGE=0
