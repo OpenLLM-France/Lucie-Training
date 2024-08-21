acad8d1cb601dae2abdf18671cafdc328e14a558
Oligou <oliviergouvert@gmail.com>
2024-08-21 14:43:04 +0200

    Update pretraining script on h100

--- training/set_env.sh
+++ training/set_env.sh
@@ -2,12 +2,14 @@
 
 # Environement
 module purge
-module load cpuarch/amd 
-module load anaconda-py3/2023.09 # Use this anaconda version already installed in Jean-Zay. If you're not on Jean-Zay, you have to install it.
-module load cuda/12.1.0 # Use this cuda version already installed in Jean-Zay. If you're not on Jean-Zay, you have to install it.
+module load anaconda-py3/2024.06
+module load cuda/12.1.0
 module load gcc/12.2.0
-conda activate lucie
+conda activate /lustre/fswork/projects/rech/fwx/commun/Lucie_h100
 
 # Variables
-export OUTPUT_PATH=/gpfswork/rech/qgz/uzq54wg
+export CHECKPOINT_PATH=$ALL_CCFRSCRATCH/checkpoints/
+export TENSORBOARD_PATH=$ALL_CCFRSCRATCH/tensorboard/
+export LOGS_PATH=$ALL_CCFRSCRATCH/lucie-logs/
+
 export MEGATRON_DEEPSPEED_REPO=/linkhome/rech/gendjf01/uzq54wg/Lucie-Training/Megatron-DeepSpeed
