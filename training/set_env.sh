313b39333c3d0c00e12a10befcbaacc526c83fe6
Oligou <oliviergouvert@gmail.com>
2024-08-21 16:08:39 +0200

    Correct variable definition

--- training/set_env.sh
+++ training/set_env.sh
@@ -6,10 +6,3 @@ module load anaconda-py3/2024.06
 module load cuda/12.1.0
 module load gcc/12.2.0
 conda activate /lustre/fswork/projects/rech/fwx/commun/Lucie_h100
-
-# Variables
-export CHECKPOINT_PATH=$ALL_CCFRSCRATCH/checkpoints/
-export TENSORBOARD_PATH=$ALL_CCFRSCRATCH/tensorboard/
-export LOGS_PATH=$ALL_CCFRSCRATCH/lucie-logs/
-
-export MEGATRON_DEEPSPEED_REPO=/linkhome/rech/gendjf01/uzq54wg/Lucie-Training/Megatron-DeepSpeed
