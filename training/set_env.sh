98792a1a9015dcf613ff951b1ce6145ca8ecb174
Jeronymous <jeromelouradour@gmail.com>
2025-01-13 13:11:40 +0100

    remove hardcoded absolute paths (centralize everything in the set_env.sh file

--- training/set_env.sh
+++ training/set_env.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Environement
+# Jean-Zay Modules
 module purge
 module load arch/h100
 module load anaconda-py3/2024.06
 module load cuda/12.1.0
 module load gcc/12.2.0
+
+# Python Environment
 conda activate /lustre/fswork/projects/rech/fwx/commun/Lucie_h100
+
+# Environment Variables
+# - source
+export SRC_DIR="/linkhome/rech/xxxxxx00/xxx00xx/Lucie-Training"
+export MEGATRON_DEEPSPEED_REPO="$SRC_DIR/Megatron-DeepSpeed"
+export ASSET_DIR="$SRC_DIR/assets"
+# - (tokenized) data
+export TOKENS_DATA_DIR="$ALL_CCFRSCRATCH/preprocessed_data/Lucie/lucie_tokens_65k_grouped"
+# - output model (and training logs)
+export OUTPUT_DIR="$ALL_CCFRSCRATCH/trained_models/Lucie"
